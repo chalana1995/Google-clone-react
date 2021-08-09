7490793f98029e233b8e5fdad6a1fd94ae626065
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-08-09 12:26:53 +0530

    create serch page

---
+++ google-clone/pages/search.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+function Search() {
+    return (
+        <div>
+            <Head>
+              <title>Search Result</title>
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            {/* Header */}
+
+            {/* Search */}
+        </div>
+    )
+}
+
+export default Search
